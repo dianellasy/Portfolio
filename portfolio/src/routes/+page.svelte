5eeb315790ed4bc36c651cc6061ddcc2b3872f5e
Dianella Sy <dianellabsy@csu.fullerton.edu>
2025-05-24 21:16:15 -0700

    Moved LinkedIn and GitHub Icon to the Top Right Corner

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -15,39 +15,43 @@
         height: auto;   /* Preserve the image's aspect ratio automatically */
     }
 
-    .top-left {
-        /* Define styles for the clickable image positioned at the top left */
+    .top-right {
+        /* Define styles for the clickable image positioned at the top right */
         position: absolute; /* Position it relative to the viewport */
         top: 0; /* Align to the top edge */
-        left: 0;    /* Align to the left edge */
+        right: 0;    /* Align to the right edge */
         margin: 10px;   /* Add spacing from the top and left edges */
         display: flex;  /* Use flexbox layout to arrange items horizontally */
         align-items: center;    /* Vertically align the images inside the container */
     }
 
-    .top-left img {
-        /* Define styles for the image inside the top-left clickable element */
+    .github-icon {
+        /* Define styles for the GitHub icon */
+        max-width: 45px;   /* Limit the image width to 45px */
+        width: 100%;    /* Make the image responsive by scaling to 100% of its container */
+        height: auto;   /* Preserve the image's aspect ratio automatically */
+        cursor: pointer;    /* Change the cursor on hover to indicate clickability */
+        margin-left: 15px;  /* Add a left margin of 15px */
+    }
+
+    .linkedin-icon {
+        /* Define styles for the LinkedIn icon */
         max-width: 65px;   /* Limit the image width to 65px */
         width: 100%;    /* Make the image responsive by scaling to 100% of its container */
         height: auto;   /* Preserve the image's aspect ratio automatically */
         cursor: pointer;    /* Change the cursor on hover to indicate clickability */
     }
-
-    .top-left a + a {
-        /* Targets any anchor that immediately follows another */
-        margin-left: 10px;  /* Add a left margin of 10px */
-    }
 </style>
 
 
-<div class="top-left">
-    <!-- Container for the two clickable images in the top-left corner -->
+<div class="top-right">
+    <!-- Container for the two clickable images in the top-right corner -->
     <a 
         href="https://www.linkedin.com/in/dianellasy/"
         target="_blank"
         rel="noopener noreferrer"
     >
-        <img src="linkedin_icon.jpg" alt="Linkedin Icon" />
+        <img class="linkedin-icon" src="linkedin_icon.jpg" alt="Linkedin Icon" />
     </a>
 
     <a 
@@ -55,7 +59,7 @@
         target="_blank"
         rel="noopener noreferrer"
     >
-        <img src="github_icon.png" alt="GitHub Icon" />
+        <img class="github-icon" src="github_icon.png" alt="GitHub Icon" />
     </a>
 </div>
 
